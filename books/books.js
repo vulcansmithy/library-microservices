040f2a88ba40e897677a328509033cefc4aec78c
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-12-20 17:41:40 +0800

    Added Mongoose connectivity to MongoDB remote db.

--- books/books.js
+++ books/books.js
@@ -2,8 +2,15 @@
 const express = require("express");
 const app = express();
 
+// Load mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://ulegaspi:jeQki1-vizdej-dugkih@cluster0.stndn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", () => {
+    console.log("Database is connected!");
+});
+
 app.get("/", (req, res) => {
-  res.send("This is our main endpoint!");
+    res.send("This is our main endpoint!");
 })
 
 app.listen(4545, () => {
