293d1f494f2abcc5c6a8bfb155190f08810e3a13
VulcanSmithy <ulysses.legaspi@gmail.com>
2022-01-03 12:50:39 +0800

    Added code to save the passed json data, save it to a model and persist it to the remote MongoDB db.

--- books/books.js
+++ books/books.js
@@ -7,6 +7,11 @@ app.use(bodyParser.json());
 
 // Load mongoose
 const mongoose = require("mongoose");
+const res = require("express/lib/response");
+
+// Load Book model
+require("./Book");
+const Book = mongoose.model("Book");
 
 // connect
 mongoose.connect("mongodb+srv://ulegaspi:jeQki1-vizdej-dugkih@cluster0.stndn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", () => {
@@ -18,8 +23,24 @@ app.get("/", (req, res) => {
 })
 
 app.post("/book", (req, res) => {
-    console.log(req.body);
-    res.send("Testing our book routes!");
+    var newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        numberOfPages: req.body.numberOfPages,
+        publisher: req.body.publisher
+    }
+
+    // Create a new book
+    var book = new Book(newBook)
+
+    book.save().then(() => {
+        console.log("New book created!")
+    }).catch((err) => {
+        if (err) {
+            throw err;
+        }
+    })
+    res.send("A new book created with success!")
 })
 
 app.listen(4545, () => {
